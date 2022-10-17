cda29f825d1f428e7a435c4a5c27e862fd364528
jayantwaldia <imjayantwaldia@gmail.com>
2022-10-17 08:05:57 +0530

    made more responsive

--- src/components/elements/TrackHeading.js
+++ src/components/elements/TrackHeading.js
@@ -2,18 +2,18 @@ import React from "react";
 
 function TrackHeading() {
   return (
-    <div className="flex items-center bg-slate-200 py-5">
+    <div className="flex items-center bg-slate-200 py-5 ">
       <div className="w-[20%]">
-        <p className="text-center font-bold">Candidate Name</p>
+        <p className="text-center font-bold text-md md:text-lg">Candidate</p>
       </div>
-      <div className="w-[80%] flex justify-between px-5">
+      <div className="w-[80%] flex justify-between px-5 text-sm md:text-md">
         <div>Referred</div>
         <div>Interviewed</div>
         <div>Hired</div>
         <div>Joined</div>
       </div>
       <div className="w-[20%]">
-        <p className="text-center font-bold">Reward</p>
+        <p className="text-center font-bold text-md md:text-lg">Reward</p>
       </div>
     </div>
   );
--- src/components/layout/Mainview.js
+++ src/components/layout/Mainview.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Mainview({ children }) {
   return (
-    <div className="px-1 md:px-10 pt-5 h-full mx-auto max-w-10xl bg-green-500">
+    <div className="px-1 md:px-10 pt-5 h-full mx-auto max-w-10xl bg-green-500 overflow-hidden">
       {children}
     </div>
   );
